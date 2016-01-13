2f760fec1e299bd022d8502f250c6ce43919fc5b
kfujima0308 <kfujima0308@gmail.com>
2016-01-14 02:07:54 +0900

    LEE-3: Log in function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'video_contents/index'
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'video_contents/index'
   resources :video_contents
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'video_contents#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110082317) do
+ActiveRecord::Schema.define(version: 20160113122652) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "profile_image"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "video_contents", force: :cascade do |t|
     t.string   "videoUrl"
